e543945ea6df4f6cca4a2282b5aec6f9d97a36dd
iamsondev <sondipkumarsk@gmail.com>
2025-04-28 17:47:57 +0600

    added Home root component and make route

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,18 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router";
+import Root from './Layout/Root/Root.jsx';
+import Home from './Components/Home/Home.jsx';
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component:Root,
+    children: [
+      {
+        index:true,
+        Component:Home
+      }
+    ]
   },
 ]);
 
